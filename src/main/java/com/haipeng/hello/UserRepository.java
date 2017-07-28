a4c056ca516cf55d5fa79c9f15507951e29130e0
孙宜岩 <yysun@wanin.net>
2017-07-28 16:30:37 +0800

    数据插入和查询

---
+++ src/main/java/com/haipeng/hello/UserRepository.java
@@ -0,0 +1,9 @@
+package com.haipeng.hello;
+
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete and methods
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
---
+++ src/main/java/com/haipeng/user/SuperUserRepository.java
@@ -0,0 +1,6 @@
+package com.haipeng.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SuperUserRepository extends CrudRepository<SuperUser,Long>{
+}
