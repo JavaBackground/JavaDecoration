6ade1e71725a720f849e35eed99b87b392f71f44
sunyiyan <sunyiyan123@gmail.com>
2017-08-24 00:01:59 +0800

    order is ok

--- src/main/java/com/haipeng/bussiness/data/respository/UserOrderRepository.java
+++ src/main/java/com/haipeng/bussiness/data/respository/UserOrderRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface UserOrderRepository extends CrudRepository<UserOrder,Long>{
 
-    @Query(value = "select o.* from user_order o where u.user_unique_number =?1",nativeQuery = true)
+    @Query(value = "select o.* from user_order o where o.user_unique_number =?1",nativeQuery = true)
     List<UserOrder> getOrdersByUserUniqueNumnber(long uniqueNumber);
 
-    @Query(value = "select o.* from user_order o where u.unique_number =?1",nativeQuery = true)
+    @Query(value = "select o.* from user_order o where o.unique_number =?1",nativeQuery = true)
     UserOrder getOrderByUniqueNumber(long userUniqueNumber);
 }
